556b2a4c22cd84795628c0dcbf465d13cc438017
Jackson Hayes <acct-prtzl@protonmail.com>
2021-11-19 08:30:05 -0600

    Update Gibbs Example, creae file without plots

--- Modules/Module2/src/gibbs-free-energy/gibbs-free-energy-withoutplots.jl
+++ Modules/Module2/src/gibbs-free-energy/gibbs-free-energy-withoutplots.jl
@@ -1,4 +1,3 @@
-using Plots
 R = 8.314
 
 function main()
@@ -12,13 +11,9 @@ function main()
                 calculateGibbsFreeEnery()
             elseif choice == "EQC"
                 calculateEquillibriumConstant()
-            elseif choice == "PEC"
-                plotKpAtTemp()
             end
         catch e
             println("Error, Check input and try again")
-            
-            println(e)
         end 
     end 
 
@@ -29,7 +24,7 @@ function readFunctionChoice()
     println("Spontenaity Temperature Range: STR")
     println("Calculate Gibbs Free Energy: GFE")
     println("Calculate Equillibrium Constant: EQC")
-    println("Plot Equillibrium Constant For Temperature Range: PEC")
+    println("Plot Gibbs Free Energy At Temperature Range: PGT")
 
     
     println("exit: EXIT")
@@ -86,47 +81,13 @@ end
 function calculateEquillibriumConstant()
     ΔG, T = calculateGibbsFreeEnery()
     
-    Kp = computeKp(ΔG, T)
+    Kp = exp(-(ΔG*1000)/(R*T))
     println("Results of Equillibrium Constant Calculation: ")
     println(Kp)
     println("-----------------------------")
     println("")
     
 end 
-
-function computeKp(ΔG, T)
-    return exp(-(ΔG*1000)/(R*T))
-end
-
-function plotKpAtTemp()
-    ΔH, ΔS = getThermodynamicValues()
-
-    print("Enter beginning temp T1 in K: ")
-    T1 = tryparse(Float64, readline())
-    print("Enter beginning temp T2 in K: ")
-    T2 = tryparse(Float64, readline())
-
-   
-    if T1 >= T2
-        print("T2 must be larger than T1 ")
-        return 
-    end
-    T = T1
-    equilibriumConstants = Float64[]
-    temperatures = Float64[]
-    while T <= T2 
-        ΔG = computeGibbsFreeEnergy(ΔH, T, ΔS)
-        push!(equilibriumConstants, computeKp(ΔG, T)) 
-        push!(temperatures, T)
-        T += 1
-    end 
-    
-    chart = plot(temperatures, equilibriumConstants, fmt = :png)
-    
-    println("Plot Complete, Enter File Name")
-    fileName = readline()
-    savefig(chart,"./$fileName.png")
-end
 
 function getThermodynamicValues()
     print("Enter ΔH in kJ/mol: ")
